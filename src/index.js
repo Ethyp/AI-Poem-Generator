40e67feabf2be7df28fce90311b7617bac2f192f
Ethyp <epmwenga@gmail.com>
2025-08-30 22:15:23 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = "I wandered lonely as a cloud";
+}
+
+let poemForm = document.querySelector("#poem-generator-form");
+poemForm.addEventListener("submit", generatePoem);
